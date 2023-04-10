8eb7ec4fa74bb5e3ae01b52b2b6ad1c53c5128d4
HieuT23511 <hieut23511@gmail.com>
2023-04-10 11:22:03 +0700

    InterfaceComparable update

--- 2.InterfaceComparable/main.ts
+++ 2.InterfaceComparable/main.ts
@@ -1,11 +1,11 @@
 import {ComparableCircle} from "./ComparableCircle";
 
-let circle = [new ComparableCircle(10),new ComparableCircle(20)];
+let circle = [new ComparableCircle(10),new ComparableCircle(10)];
 let resultCompare = circle[0].compareTo(circle[1]);
 if(resultCompare ===1){
     console.log(`The circle with radius = ${circle[0].getRadius()} is larger the circle with radius ${circle[1].getRadius()} `)
-}else if (resultCompare !==1){
+}else if (resultCompare ===-1){
     console.log(`The circle with radius = ${circle[0].getRadius()} is smaller the circle with radius ${circle[1].getRadius()}`)
-}else {
-    console.log(`Two circle is equal size`);
+}else if(resultCompare ===0) {
+    console.log(`Two circle is equal size!`);
 }
